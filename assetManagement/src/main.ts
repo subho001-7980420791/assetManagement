6428764bb45dae9478e427a2ab2e6544b119add8
Awanish Ranjan Rai <awanish.ranjanrai@ingredion.com>
2023-08-17 18:50:13 +0530

    installed

--- assetManagement/src/main.ts
+++ assetManagement/src/main.ts
@@ -1,7 +1,22 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {Gatekeeper} from 'gatekeeper-client-sdk';
 
-import { AppModule } from './app/app.module';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
+if (environment.production) {
+    enableProdMode();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+(window as any).PF = {
+    config: {
+        mode: 'bs4'
+    }
+};
+
+Gatekeeper.initialize('9966bf1b-5da5-4b55-9301-86f9f0c77aaf');
+
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
