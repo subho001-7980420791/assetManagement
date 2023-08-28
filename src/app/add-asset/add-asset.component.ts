602e78308f29142cc18f95abc983b8c540112ae4
swagatapal123 <swagata.pal@blucocoondigital.com>
2023-08-28 14:54:57 +0530

    ooo

--- src/app/add-asset/add-asset.component.ts
+++ src/app/add-asset/add-asset.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { CountryService } from '@services/country.service';
 import { BackendService } from '@services/backend.service';
 import  pdfMake from 'pdfmake/build/pdfmake';
 import pdfFonts from 'pdfmake/build/vfs_fonts';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-add-asset',
@@ -11,7 +12,9 @@ import pdfFonts from 'pdfmake/build/vfs_fonts';
   styleUrls: ['./add-asset.component.scss']
 })
 export class AddAssetComponent implements OnInit{
+
   dataSource:any[]=[];
+  importedData: any[]=[];
   showPopup:boolean=false
   users:any[]=[]
   country:any[]=[]
@@ -24,7 +27,8 @@ export class AddAssetComponent implements OnInit{
       assetModelId:['',Validators.required],
       allocated:[true,Validators.required],
       isActive:[true,Validators.required],
-      buildingId:['',Validators.required]
+      buildingId:['',Validators.required],
+      readableBy:['',Validators.required]
   })
   constructor(public countryService:CountryService,private fb: FormBuilder,private backEndService:BackendService) {
    }
@@ -43,8 +47,7 @@ export class AddAssetComponent implements OnInit{
     this.asset.reset()
   }
   async onSubmit(){
-      this.dataSource.push(this.asset.getRawValue())
-      console.log(this.asset.getRawValue())
+      this.dataSource.unshift(this.asset.getRawValue())
       await this.backEndService.makePostApiCall('asset',this.asset.getRawValue())
       this.asset.reset()
       this.countryService.allocatedRoomId=[]
@@ -201,4 +204,18 @@ export class AddAssetComponent implements OnInit{
       this.country=data.data
     }
   }
+
+  // readExcel(event:any){
+  //   let file = event.target.files[0];
+  //   let fileReader = new FileReader();
+  //   fileReader.readAsBinaryString(file);
+  //   fileReader.onload = (e)=>{
+  //     var workBook = XLSX.read(fileReader.result,{type:'binary'});
+  //     var sheetNames = workBook.SheetNames;
+  //     this.importedData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]);
+  //     console.log(this.importedData);
+  //   }
+  // }
+
+
 }
